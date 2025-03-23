94899e5881f38e1ad81c6eecd128d6a41c8f3a7d
sk-deku <kesavansivakumar50@gmail.com>
2025-03-23 17:32:15 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,6 +15,7 @@ SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ['API_ID'])
 API_HASH = environ['API_HASH']
 BOT_TOKEN = environ['BOT_TOKEN']
+BOT_USERNAME = "Luffy_Anime_Filter_bot"
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
